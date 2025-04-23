554846e0c93229b0ddc67068d69d163cdb141407
Fábio de Jesus <142427321+fabiodejesusfferreira@users.noreply.github.com>
2025-04-23 20:13:39 -0300

    Added public routes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 import express from "express";
+import publicRoutes from "./routes/public.js"
 
 const app = express();
+app.use(express.json())
+app.use('/', publicRoutes) 
 const port = 8080;
-app.listen(port, console.log(`Server running on port ${port}`));
- 
+
+app.listen(port, console.log(`Server running on port ${port}`)); 
