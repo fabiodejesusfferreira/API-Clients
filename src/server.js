c8dbc62bbd327629f5696902c4cc344061ed5798
Fábio de Jesus <142427321+fabiodejesusfferreira@users.noreply.github.com>
2025-04-27 20:05:35 -0300

    Configuration for connecting to the database

--- src/server.js
+++ src/server.js
@@ -1,9 +1,17 @@
 import express from "express";
 import publicRoutes from "./routes/public.js"
+import mongoose from 'mongoose';
+import config from "../config.js"
 
 const app = express();
 app.use(express.json())
 app.use('/', publicRoutes) 
 const port = 8080;
+
+await mongoose.connect(
+    config.MongoDB_URL
+)
+.then(() => console.log(`MongoDB successfully connected`))
+.catch((err) => console.error('An error occurred when connecting to the database', err))
 
 app.listen(port, console.log(`Server running on port ${port}`)); 
