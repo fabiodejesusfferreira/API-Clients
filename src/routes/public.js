2107ec486bf80511fd8656691b3e3404556efb60
Fábio de Jesus <142427321+fabiodejesusfferreira@users.noreply.github.com>
2025-04-27 20:28:29 -0300

    Added login route
    In addition, the password is already encrypted and can be decrypted
    And a token has also been added using JsonWebToken

--- src/routes/public.js
+++ src/routes/public.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
-import { genSalt, hash } from "bcrypt";
+import { compare, genSalt, hash } from "bcrypt";
 import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+import config from "../../config.js";
 
 const router = Router();
 
@@ -8,8 +10,9 @@ router.post("/register", async (req, res) => {
     try {
         const user = req.body;
 
-        let userToVerify = await User.findOne({ email: user.email })
-        if(userToVerify) return res.status(403).json(`This user already exists!`)
+        let userToVerify = await User.findOne({ email: user.email });
+        if (userToVerify)
+            return res.status(403).json(`This user already exists!`);
 
         const salt = await genSalt(10);
         const hashPassword = await hash(user.password, salt);
@@ -44,6 +47,30 @@ router.post("/register", async (req, res) => {
     }
 });
 
+router.post("/login", async (req, res) => {
+    try {
+        const userInfo = req.body;
 
+        const user = await User.findOne({ email: userInfo.email });
+
+        if (!user) return res.status(404).json({ message: `User not found` });
+
+        const isMatch = await compare(userInfo.password, user.password);
+
+        if (!isMatch)
+            return res
+                .status(400)
+                .json({ message: "invalid email or password" });
+
+        const token = jwt.sign({ id: user.id }, config.JWT_SECRET, {
+            expiresIn: "3d",
+        });
+
+        return res.status(200).json(token);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: `Error when trying to login` });
+    }
+});
 
 export default router;
