f3988ab0479cf194289355e9a673073eb7435288
Fábio de Jesus <142427321+fabiodejesusfferreira@users.noreply.github.com>
2025-04-27 20:49:27 -0300

    Added private routes and middleware

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 import express from "express";
 import publicRoutes from "./routes/public.js"
+import privateRoutes from "./routes/private.js"
 import mongoose from 'mongoose';
 import config from "../config.js"
+import auth from './middlewares/auth.js';
 
 const app = express();
 app.use(express.json())
-app.use('/', publicRoutes) 
+app.use('/', publicRoutes)
+app.use('/', auth, privateRoutes)
 const port = 8080;
 
 await mongoose.connect(
